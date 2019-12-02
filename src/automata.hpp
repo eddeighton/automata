35d46030f01c80161b331b756feb82aa8a352ea1
Ed <eddeighton@hotmail.co.uk>
2019-12-02 06:28:37 +0000

    fun with grids

--- src/automata.hpp
+++ src/automata.hpp
@@ -36,8 +36,19 @@ namespace automata
     template< std::size_t Size >
     inline std::int32_t gridMoveState( State szState, State szHori, State szVert )
     {
+        
+        if( ( szState % Size  ) + szHori < 0 )
+            return DEAD_STATE;
+        if( ( szState % Size  ) + szHori >= Size )
+            return DEAD_STATE;
+        
+        if( ( szState / Size  ) + szVert < 0 )
+            return DEAD_STATE;
+        if( ( szState / Size  ) + szVert >= Size )
+            return DEAD_STATE;
+        
         State sz = szState + szHori + szVert * Size;
-        if( sz >=0 && sz < ( Size * Size ) )
+        if( ( sz >= 0 ) && sz < ( Size * Size ) )
             return sz;
         else
             return DEAD_STATE;
@@ -53,15 +64,15 @@ namespace automata
         {
             for( State y = 0; y < Size; ++y )
             {
-                const State szState = gridMoveState( 0, x, y );
-                if( szState >=0 && szState < ( Size * Size ) )
+                const State szState = gridMoveState< Size >( 0, x, y );
+                if( szState >= 0 && szState < ( Size * Size ) )
                 {
                     typename Table::Actions& actions = table[ szState ];
                     
-                    actions[ eUp ]      = gridMoveState( szState, 0, 1 );
-                    actions[ eDown ]    = gridMoveState( szState, 0, -1 );
-                    actions[ eLeft ]    = gridMoveState( szState, -1, 0 );
-                    actions[ eRight ]   = gridMoveState( szState, 1, 0 );
+                    actions[ eUp ]      = gridMoveState< Size >( szState, 0, 1 );
+                    actions[ eDown ]    = gridMoveState< Size >( szState, 0, -1 );
+                    actions[ eLeft ]    = gridMoveState< Size >( szState, -1, 0 );
+                    actions[ eRight ]   = gridMoveState< Size >( szState, 1, 0 );
                 }
             }
         }
